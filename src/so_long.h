4e75e131e10108e73a2e28264e544c6dc4a7e41e
MyNameIsTrez <sbos@student.codam.nl>
2022-07-01 18:26:26 +0200

    Remove redundant header stuff.

--- src/so_long.h
+++ src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/15 16:21:23 by sbos          #+#    #+#                 */
-/*   Updated: 2022/07/01 18:23:18 by sbos          ########   odam.nl         */
+/*   Updated: 2022/07/01 18:25:51 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,13 +26,13 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-# include "a_utils.h"
-# include "iterators/iterators.h"
+# include "sl_settings.h"
+# include "sl_structs.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 
-# include "sl_settings.h"
-# include "sl_structs.h"
+# include "a_utils.h"
+# include "iterators/iterators.h"
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -42,6 +42,7 @@ t_status	sl_instantiate_tile_kinds(t_data *data);
 t_status	sl_instantiate_tile_grid(t_data *data);
 t_status	sl_instantiate_entities(t_data *data);
 t_status	sl_instantiate_players(t_data *data);
+
 void		sl_cleanup(t_data *data);
 
 ////////////////////////////////////////////////////////////////////////////////
