eed1413742b9261b40edf6f11a7e2573742d3cd8
MyNameIsTrez <sbos@student.codam.nl>
2022-07-14 11:38:11 +0200

    Refactor update_wall_frames() a bit

--- src/frames/update_frames.c
+++ src/frames/update_frames.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/12 11:00:12 by sbos          #+#    #+#                 */
-/*   Updated: 2022/07/13 17:03:11 by sbos          ########   odam.nl         */
+/*   Updated: 2022/07/14 11:36:43 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,16 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-STATIC void	update_wall_frames(mlx_image_t *frame, t_data *data)
+STATIC void	update_wall_frames(unsigned char tile_kind_character, t_data *data)
 {
 	static t_color_step	color_step = {-1, -1, -1};
 	uint32_t			pixel_index;
 	uint32_t			pixels_data_index;
+	t_tile_kind			*tile_kind;
+	mlx_image_t			*frame;
 
+	tile_kind = &data->tile_kinds[tile_kind_character];
+	frame = tile_kind->frames[0];
 	while (sl_iterate_frame_pixels(frame, data) != FINISHED)
 	{
 		pixel_index = sl_get_pixel_index(frame, data);
@@ -57,7 +61,7 @@ STATIC void	update_wall_frames(mlx_image_t *frame, t_data *data)
 
 void	sl_update_frames(t_data *data)
 {
-	update_wall_frames(data->tile_kinds[WALL_CHARACTER].frames[0], data);
+	update_wall_frames(WALL_CHARACTER, data);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- src/settings/sl_settings_tiles.h
+++ src/settings/sl_settings_tiles.h
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/01 17:51:47 by sbos          #+#    #+#                 */
-/*   Updated: 2022/07/13 16:32:21 by sbos          ########   odam.nl         */
+/*   Updated: 2022/07/14 11:32:59 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,54 @@
 # define EMPTY_SPACE_TEXTURE_ROW 0
 # define EMPTY_SPACE_CHARACTER '0'
 
+# define EMPTY_SPACE_R 100
+# define EMPTY_SPACE_G 100
+# define EMPTY_SPACE_B 100
+# define EMPTY_SPACE_A 255
+
+////////////////////////////////////////////////////////////////////////////////
+
 # define WALL_TEXTURE_FRAME_COUNT 1
 # define WALL_TEXTURE_ROW 3
 # define WALL_CHARACTER '1'
+
+# define WALL_R 100
+# define WALL_G 100
+# define WALL_B 100
+# define WALL_A 255
+
+////////////////////////////////////////////////////////////////////////////////
 
 # define COLLECTIBLE_TEXTURE_FRAME_COUNT 4
 # define COLLECTIBLE_TEXTURE_ROW 46
 # define COLLECTIBLE_CHARACTER 'C'
 
+# define COLLECTIBLE_R 100
+# define COLLECTIBLE_G 100
+# define COLLECTIBLE_B 100
+# define COLLECTIBLE_A 255
+
+////////////////////////////////////////////////////////////////////////////////
+
 # define MAP_EXIT_TEXTURE_FRAME_COUNT 1
 # define MAP_EXIT_TEXTURE_ROW 18
 # define MAP_EXIT_CHARACTER 'E'
 
+# define MAP_EXIT_R 100
+# define MAP_EXIT_G 100
+# define MAP_EXIT_B 100
+# define MAP_EXIT_A 255
+
+////////////////////////////////////////////////////////////////////////////////
+
 # define PLAYER_TEXTURE_FRAME_COUNT 4
 # define PLAYER_TEXTURE_ROW 25
 # define PLAYER_CHARACTER 'P'
+
+# define PLAYER_R 100
+# define PLAYER_G 100
+# define PLAYER_B 100
+# define PLAYER_A 255
 
 ////////////////////////////////////////////////////////////////////////////////
 
