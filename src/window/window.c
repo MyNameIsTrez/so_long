a101ae7e94f4652944dfffca1890cfeb651ed709
MyNameIsTrez <sbos@student.codam.nl>
2022-07-29 14:24:21 +0200

    Split sl_calculate_window_width_and_height() up into two subfunctions

--- src/window/window.c
+++ src/window/window.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/29 14:19:19 by sbos          #+#    #+#                 */
-/*   Updated: 2022/07/29 14:19:24 by sbos          ########   odam.nl         */
+/*   Updated: 2022/07/29 14:23:09 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,29 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
+STATIC void	sl_calculate_window_width(size_t pixels_per_tile, t_data *data)
+{
+	data->window.width = data->char_grid.width * pixels_per_tile;
+	if (data->window.width > data->monitor.width)
+		data->window.width = data->monitor.width;
+}
+
+STATIC void	sl_calculate_window_height(size_t pixels_per_tile, t_data *data)
+{
+	data->window.height = data->char_grid.height * pixels_per_tile;
+	if (data->window.height > data->monitor.height)
+		data->window.height = data->monitor.height;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 void	sl_calculate_window_width_and_height(t_data *data)
 {
 	size_t	pixels_per_tile;
 
 	pixels_per_tile = data->texture.pixels_per_tile;
-	data->window.width = data->char_grid.width * pixels_per_tile;
-	if (data->window.width > data->monitor.width)
-		data->window.width = data->monitor.width;
-	data->window.height = data->char_grid.height * pixels_per_tile;
-	if (data->window.height > data->monitor.height)
-		data->window.height = data->monitor.height;
+	sl_calculate_window_width(pixels_per_tile, data);
+	sl_calculate_window_height(pixels_per_tile, data);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
