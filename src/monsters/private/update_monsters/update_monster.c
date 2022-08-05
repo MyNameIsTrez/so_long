211df6cb60a3dbe3ba1d6a9f0784b588807e10f6
MyNameIsTrez <sbos@student.codam.nl>
2022-08-05 21:47:45 +0200

    Make monsters never stop for a frame

--- src/monsters/private/update_monsters/update_monster.c
+++ src/monsters/private/update_monsters/update_monster.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/04 14:42:08 by sbos          #+#    #+#                 */
-/*   Updated: 2022/08/05 21:40:18 by sbos          ########   odam.nl         */
+/*   Updated: 2022/08/05 21:43:43 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,25 @@
 
 void	sl_update_monster(t_monster *monster, t_data *data)
 {
-	t_heading	heading;
 	t_tile		*tile;
 	t_i32		column;
 	t_i32		row;
 
-	heading = monster->heading;
-	if (heading == HEADING_NONE)
+	if (monster->heading == HEADING_NONE)
 		sl_set_monster_heading(monster, data);
-	heading = monster->heading;
-	if (heading != HEADING_NONE)
+	if (monster->heading != HEADING_NONE)
 	{
 		tile = &monster->entity->tile;
-		column = sl_get_heading_column(heading, tile);
-		row = sl_get_heading_row(heading, tile);
-		if (can_walk(column, row, data))
+		column = sl_get_heading_column(monster->heading, tile);
+		row = sl_get_heading_row(monster->heading, tile);
+		if (!can_walk(column, row, data))
+			sl_set_monster_heading(monster, data);
+		if (monster->heading != HEADING_NONE)
+		{
+			column = sl_get_heading_column(monster->heading, tile);
+			row = sl_get_heading_row(monster->heading, tile);
 			sl_set_tile_pos(tile, (size_t)column, (size_t)row, data);
-		else
-			monster->heading = HEADING_NONE;
+		}
 	}
 }
 
