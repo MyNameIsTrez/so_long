5246dc0b9f3d4e04d186f0e97283c1298dc9f700
MyNameIsTrez <sbos@student.codam.nl>
2022-08-05 21:17:32 +0200

    Update entities every N ticks

--- src/monsters/update_monsters.c
+++ src/monsters/update_monsters.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/04 15:57:22 by sbos          #+#    #+#                 */
-/*   Updated: 2022/08/05 20:08:05 by sbos          ########   odam.nl         */
+/*   Updated: 2022/08/05 21:15:33 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,22 @@
 
 void	sl_update_monsters(t_data *data)
 {
+	t_monster	*monster;
+	t_entity	*entity;
+
 	while (sl_iterate_monsters(data) != FINISHED)
-		sl_update_monster(data->it.monster, data);
+	{
+		monster = data->it.monster;
+		entity = monster->entity;
+		if (entity->ticks_since_last_update > \
+			entity->ticks_between_updates)
+		{
+			sl_update_monster(monster, data);
+			entity->ticks_since_last_update = 0;
+		}
+		else
+			entity->ticks_since_last_update++;
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
