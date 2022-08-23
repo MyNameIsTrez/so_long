9cd7bc3c1d2378fb264e329d6c1a7398ace2227f
MyNameIsTrez <sbos@student.codam.nl>
2022-08-23 12:52:51 +0200

    Make monsters turn right

--- src/monsters/set_monster_heading.c
+++ src/monsters/set_monster_heading.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/05 18:45:21 by sbos          #+#    #+#                 */
-/*   Updated: 2022/08/23 12:46:36 by sbos          ########   odam.nl         */
+/*   Updated: 2022/08/23 12:52:37 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,12 @@ void	sl_set_monster_heading(t_monster *monster, t_data *data)
 	t_heading	heading;
 	t_tile		*monster_tile;
 
+	if (monster->heading == HEADING_NONE)
+		monster->heading = HEADING_UP;
 	ft_init_it(&it);
 	while (sl_iterate_headings(&it, data) != FINISHED)
 	{
-		heading = data->it.heading;
+		heading = (monster->heading + data->it.heading) % 4;
 		monster_tile = &monster->entity->tile;
 		if (can_walk(heading, monster_tile, data))
 		{
