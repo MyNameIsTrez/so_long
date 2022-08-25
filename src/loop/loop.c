cdb5cc861a01610b1e4a8476e58db121c93cc52d
MyNameIsTrez <sbos@student.codam.nl>
2022-08-25 13:52:31 +0200

    Still update held keys after having finished the level

--- src/loop/loop.c
+++ src/loop/loop.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/29 13:56:49 by sbos          #+#    #+#                 */
-/*   Updated: 2022/08/25 13:27:12 by sbos          ########   odam.nl         */
+/*   Updated: 2022/08/25 13:51:29 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,9 @@ void	sl_loop(void *param)
 	data = param;
 	if (mlx_is_key_down(data->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(data->mlx);
+	sl_update_held_keys(data);
 	if (!data->finished_level)
 	{
-		sl_update_held_keys(data);
 		sl_update_entities(data);
 		sl_change_frames(data);
 	}
